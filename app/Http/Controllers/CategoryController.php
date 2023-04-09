0ba57157e5924d15503e3e40d4a893477c3cb933
Yahu^^ <phamgiahung02@gmail.com>
2023-04-09 23:08:29 +0700

    fix categoriesList of Index page_User

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
@@ -10,85 +11,101 @@ use Illuminate\Support\Facades\Auth;
 
 class CategoryController extends Controller
 {
-    public function showAllCategories(){
-        if(Auth::guard('admin')->check()){
+    public function showAllCategories()
+    {
+        if (Auth::guard('admin')->check()) {
             $categoriesList = DB::table('categories')->select()->get();
             return view('manager.productCategories')->with(compact('categoriesList'));
-        }else{
-            return redirect()->route('admin.adminLoginForm')->with('msg','Login with admin account first, please !!!');
+        } else {
+            return redirect()->route('admin.adminLoginForm')->with('msg', 'Login with admin account first, please !!!');
         }
     }
-    public function deleteCategoryByID($id) {
-        if(Auth::guard('admin')->check()){
-            if (!empty($id)){
-                $categoryDetail = DB::table('categories')->select()->where('category_id','=',$id)->first();
-                if (!empty($categoryDetail)){
-                    $deleteStatus = DB::table('categories')->where('category_id','=',$id)->delete();
+    public function deleteCategoryByID($id)
+    {
+        if (Auth::guard('admin')->check()) {
+            if (!empty($id)) {
+                $categoryDetail = DB::table('categories')->select()->where('category_id', '=', $id)->first();
+                if (!empty($categoryDetail)) {
+                    $deleteStatus = DB::table('categories')->where('category_id', '=', $id)->delete();
                     if ($deleteStatus) {
                         $msg = 'Deleted Successful';
-                    }else {
+                    } else {
                         $msg = 'Error';
                     }
-                }else{
+                } else {
                     $msg = 'The Category is not available';
                 }
-            }else{
-               $msg = 'The connection is not available';
+            } else {
+                $msg = 'The connection is not available';
             }
-            return redirect()->route('admin.category.categories')->with('msg',$msg);
-        }else{
-            return redirect()->route('admin.adminLoginForm')->with('msg','Login with admin account first, please !!!');
+            return redirect()->route('admin.category.categories')->with('msg', $msg);
+        } else {
+            return redirect()->route('admin.adminLoginForm')->with('msg', 'Login with admin account first, please !!!');
         }
     }
-    public function getCategoryDetails($id){
-        if(Auth::guard('admin')->check()){
-            $categoryDetail = DB::table('categories')->select()->where('category_id','=',$id)->first();
+    public function getCategoryDetails($id)
+    {
+        if (Auth::guard('admin')->check()) {
+            $categoryDetail = DB::table('categories')->select()->where('category_id', '=', $id)->first();
             return view('manager.editCategory')->with(compact('categoryDetail'));
-        }else{
-            return redirect()->route('admin.adminLoginForm')->with('msg','Login with admin account first, please !!!');
+        } else {
+            return redirect()->route('admin.adminLoginForm')->with('msg', 'Login with admin account first, please !!!');
         }
     }
-    public function updateCategory(Request $request){
-       if(Auth::guard('admin')->check()){
+    public function updateCategory(Request $request)
+    {
+        if (Auth::guard('admin')->check()) {
             $newCategory = $request->all();
-            DB::table('categories')->where('category_id','=',$newCategory['category_id'])
-            ->update([
-                'category_name'=>$newCategory['category_name'],
-                'category_description'=>$newCategory['category_description'],
-            ]);
-            return redirect()->route('admin.category.categories')->with('msg','Update Category Successful');
-       }else{
-        return redirect()->route('admin.adminLoginForm')->with('msg', 'Login by Admin account first, please !!!');
-       }
+            DB::table('categories')->where('category_id', '=', $newCategory['category_id'])
+                ->update([
+                    'category_name' => $newCategory['category_name'],
+                    'category_description' => $newCategory['category_description'],
+                ]);
+            return redirect()->route('admin.category.categories')->with('msg', 'Update Category Successful');
+        } else {
+            return redirect()->route('admin.adminLoginForm')->with('msg', 'Login by Admin account first, please !!!');
+        }
     }
-    public function showAddFormCategory(){
+    public function showAddFormCategory()
+    {
         return view('manager.addCategory');
     }
-    public function createCategory(Request $request){
-        if(Auth::guard('admin')->check()){
+    public function createCategory(Request $request)
+    {
+        if (Auth::guard('admin')->check()) {
             $newCategory = $request->all();
             $newCategory_name = ($newCategory['category_name']);
             $newCategory_id = ($newCategory['category_id']);
 
             $categoriesList = DB::table('categories')->select()->get();
-            foreach($categoriesList as $key => $category){
+            foreach ($categoriesList as $key => $category) {
                 $category_id = $category->category_id;
                 $category_name = $category->category_name;
-                if(strcmp( $newCategory_id, $category_id )==0){
-                    return redirect()->route('admin.category.addCategory')->with('msg','This ID has already been register !!!');
+                if (strcmp($newCategory_id, $category_id) == 0) {
+                    return redirect()->route('admin.category.addCategory')->with('msg', 'This ID has already been register !!!');
                 }
-                if(strcmp( $newCategory_name, $category_name )==0){
-                    return redirect()->route('admin.category.addCategory')->with('msg','This Name has already been register !!!');
+                if (strcmp($newCategory_name, $category_name) == 0) {
+                    return redirect()->route('admin.category.addCategory')->with('msg', 'This Name has already been register !!!');
                 }
             }
             DB::table('categories')->insert([
-                'category_id'=>$newCategory_id,
-                'category_name'=>$newCategory_name,
-                'category_description'=>$newCategory['category_description'],
+                'category_id' => $newCategory_id,
+                'category_name' => $newCategory_name,
+                'category_description' => $newCategory['category_description'],
             ]);
-            return redirect()->route('admin.category.categories')->with('msg','Add new Category Successful');
-        }else{
+            return redirect()->route('admin.category.categories')->with('msg', 'Add new Category Successful');
+        } else {
             return redirect()->route('admin.adminLoginForm')->with('msg', 'Login by Admin account first, pls !!!');
         }
+    }
+
+
+    // User
+    public function showAllCategoriesUser()
+    {
+        $category = new Category();
+        $categoriesList = $category->getAllCategories();
+        // return view('user.index')->with(compact('categoriesList'));
+        return $categoriesList;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ use App\Models\UserAccount;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', [ProductController::class, 'index'])->name('home');
 Route::get('/contact', [ProductController::class, 'showContact'])->name('contact');
 Route::get('/blog', [BlogController::class, 'showAllBlogForUser'])->name('blog');
@@ -33,10 +34,11 @@ Route::get('/product-details/{id}', [ProductController::class, 'searchProductsBy
 
 
 
-Route::prefix('admin')->name('admin.')->group(function(){
+
+Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('', [ProductController::class, 'admin'])->name('index');
     Route::get('/adminLoginForm', [AdminAccountsController::class, 'login_form'])->name('adminLoginForm');
-    Route::post('/adminLogin',[AdminAccountsController::class,'admin_login'])->name('adminLogin');
+    Route::post('/adminLogin', [AdminAccountsController::class, 'admin_login'])->name('adminLogin');
     Route::get('/adminLogout', [AdminAccountsController::class, 'admin_logout'])->name('adminLogout');
     Route::get('/add', [ProductController::class, 'showAddForm'])->name('add');
     Route::post('/add', [ProductController::class, 'postAdd'])->name('post-add');
@@ -45,17 +47,15 @@ Route::prefix('admin')->name('admin.')->group(function(){
     Route::get('/delete/{id}', [ProductController::class, 'delete'])->name('delete');
     Route::get('/listAdmin', [AdminAccountsController::class, 'showListAdmin'])->name('listAdmin');
 
-    Route::prefix('category')->name('category.')->group(function(){
+    Route::prefix('category')->name('category.')->group(function () {
         Route::get('/', [CategoryController::class, 'showAllCategories'])->name('categories');
         Route::get('/add-category', [CategoryController::class, 'showAddFormCategory'])->name('addCategory');
         Route::post('/createCategory', [CategoryController::class, 'createCategory'])->name('createCategory');
         Route::get('/deleteCategory/{id}', [CategoryController::class, 'deleteCategoryByID'])->name('deleteCategory');
         Route::get('/editCategory/{id}', [CategoryController::class, 'getCategoryDetails'])->name('editCategory');
         Route::post('/updateCategory', [CategoryController::class, 'updateCategory'])->name('updateCategory');
-
-
     });
-    Route::prefix('blog')->name('blog.')->group(function(){
+    Route::prefix('blog')->name('blog.')->group(function () {
         Route::get('/', [BlogController::class, 'showAllBlog'])->name('index');
         Route::get('/add', [BlogController::class, 'showAddForm'])->name('add');
         Route::post('/add', [BlogController::class, 'postAdd'])->name('post-add');
@@ -64,53 +64,49 @@ Route::prefix('admin')->name('admin.')->group(function(){
         Route::post('/updateBlog', [BlogController::class, 'updateBlog'])->name('updateBlog');
         Route::get('/delete/{id}', [BlogController::class, 'delete'])->name('delete');
     });
-    Route::prefix('account')->name('account.')->group(function(){
+    Route::prefix('account')->name('account.')->group(function () {
         Route::get('/', [UserAccountController::class, 'showAllAccount'])->name('index');
-        Route::get('/set-account-disable/{id}',[UserAccountController::class,'set_Account_Disable_ByID'])->name('set-account-disable');
-        Route::get('/set-account-enable/{id}',[UserAccountController::class,'set_Account_Enable_ByID'])->name('set-account-enable');
+        Route::get('/set-account-disable/{id}', [UserAccountController::class, 'set_Account_Disable_ByID'])->name('set-account-disable');
+        Route::get('/set-account-enable/{id}', [UserAccountController::class, 'set_Account_Enable_ByID'])->name('set-account-enable');
     });
-    Route::prefix('order')->name('order.')->group(function(){
+    Route::prefix('order')->name('order.')->group(function () {
         Route::get('/', [OrdersController::class, 'showAllOrders'])->name('index');
-        Route::get('/set-status-delivering/{id}',[OrdersController::class,'set_Status_Delivering_ByID'])->name('set-status-delivering');
-        Route::get('/set-status-received/{id}',[OrdersController::class,'set_Status_Received_ByID'])->name('set-status-received');
+        Route::get('/set-status-delivering/{id}', [OrdersController::class, 'set_Status_Delivering_ByID'])->name('set-status-delivering');
+        Route::get('/set-status-received/{id}', [OrdersController::class, 'set_Status_Received_ByID'])->name('set-status-received');
     });
-    
 });
 
-Route::post('/add-cart-ajax',[AddtoCartController::class,'add_cart_ajax'])->name('add_cart_ajax');
-Route::post('/show-cart-live-view',[AddtoCartController::class,'show_cart_live_view'])->name('show_cart_live_view');
+Route::post('/add-cart-ajax', [AddtoCartController::class, 'add_cart_ajax'])->name('add_cart_ajax');
+Route::post('/show-cart-live-view', [AddtoCartController::class, 'show_cart_live_view'])->name('show_cart_live_view');
 Route::get('/cart', [AddtoCartController::class, 'showCart'])->name('cart');
-Route::post('/update-cart',[AddtoCartController::class,'update_cart']);
+Route::post('/update-cart', [AddtoCartController::class, 'update_cart']);
 Route::get('/delete-cart/{session_id}', [AddtoCartController::class, 'delete_cart_product'])->name('delete-cart');
 Route::get('/delete-all', [AddtoCartController::class, 'delete_all_product']);
 
 //Login//Logout//Register//RecoveryPassword
 Route::get('/login', [UserAccountController::class, 'showFormLogin'])->name('loginForm');
 Route::get('/register-form', [UserAccountController::class, 'showFormRegister'])->name('register-form');
-Route::post('/register',[UserAccountController::class,'registerAccount'])->name('register');
-Route::post('/userLogin',[UserAccountController::class,'userLogin'])->name('userLogin');
+Route::post('/register', [UserAccountController::class, 'registerAccount'])->name('register');
+Route::post('/userLogin', [UserAccountController::class, 'userLogin'])->name('userLogin');
 Route::get('/logout', [UserAccountController::class, 'logout'])->name('logout');
 Route::get('/forgot-password', [UserAccountController::class, 'showFormForgotPassword'])->name('forgot-password');
-Route::post('/send-verify-mail',[UserAccountController::class,'sendVerifyMail'])->name('send-verify-mail');
+Route::post('/send-verify-mail', [UserAccountController::class, 'sendVerifyMail'])->name('send-verify-mail');
 Route::get('/password/reset/{token}', [UserAccountController::class, 'showFormResetPassword'])->name('reset.password.form');
-Route::post('/reset',[UserAccountController::class,'resetPassword'])->name('reset.password');
+Route::post('/reset', [UserAccountController::class, 'resetPassword'])->name('reset.password');
 
 
 //User CRUD
-Route::get('/userProfile',[UserAccountController::class,'showUserProfile'])->name('userProfile');
-Route::get('/change-password',[UserAccountController::class,'change_password'])->name('change-password');
-Route::get('/user-orders',[UserAccountController::class,'show_history_orders'])->name('user-orders');
-Route::get('/user-vouchers',[UserAccountController::class,'show_list_vouchers'])->name('user-vouchers');
+Route::get('/userProfile', [UserAccountController::class, 'showUserProfile'])->name('userProfile');
+Route::get('/change-password', [UserAccountController::class, 'change_password'])->name('change-password');
+Route::get('/user-orders', [UserAccountController::class, 'show_history_orders'])->name('user-orders');
+Route::get('/user-vouchers', [UserAccountController::class, 'show_list_vouchers'])->name('user-vouchers');
 Route::get('/delete-account/{id}', [UserAccountController::class, 'deleteAccountByID'])->name('delete-account');
-Route::post('/update-profile',[UserAccountController::class,'update_profile'])->name('update-profile');
-Route::post('/update-password',[UserAccountController::class,'update_password'])->name('update-password');
-Route::get('/cancelOrder/{id}',[UserAccountController::class,'cancelOrderByID'])->name('cancelOrder');
+Route::post('/update-profile', [UserAccountController::class, 'update_profile'])->name('update-profile');
+Route::post('/update-password', [UserAccountController::class, 'update_password'])->name('update-password');
+Route::get('/cancelOrder/{id}', [UserAccountController::class, 'cancelOrderByID'])->name('cancelOrder');
 
 
 //Check out
 Route::get('/check-out', [OrdersController::class, 'showCheckOutForm'])->name('checkout');
 Route::post('/confirm-checkout', [OrdersController::class, 'confirm_checkout'])->name('confirm-checkout');
 Route::get('/confirm-success', [OrdersController::class, 'confirm_success'])->name('confirm-success');
-
-
-
